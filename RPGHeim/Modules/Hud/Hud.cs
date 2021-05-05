ff5684209c892e92fa949e863d163cf730a36222
atravotum <44779531+atravotum@users.noreply.github.com>
2021-05-05 11:56:25 -0500

    Update Hud.cs

--- RPGHeim/Modules/Hud/Hud.cs
+++ RPGHeim/Modules/Hud/Hud.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using HarmonyLib;
 using Object = UnityEngine.Object;
+using Jotunn.Managers;
 
 namespace RPGHeim
 {
@@ -14,13 +15,23 @@ namespace RPGHeim
             harmony.PatchAll();
         }
 
+        private static void CreateClassBar ()
+        {
+            Console.print("====================================OK OK OK OK OK OK OK +++++++++++++++++====================== HERE HRE HRE HRE HRE");
+            var sprite = GUIManager.Instance.GetSprite("item_background");
+            //var button = GUIManager.Instance.CreateButton("A Test Button", testPanel.transform, new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), new Vector2(0, 0), 250, 100);
+            var text = GUIManager.Instance.CreateText("JötunnLib, the Valheim Lib", GUIManager.PixelFix.transform, new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), new Vector2(0f, 0f), GUIManager.Instance.AveriaSerifBold, 18, GUIManager.Instance.ValheimOrange, true, Color.black, 400f, 30f, false);
+            var panel = GUIManager.Instance.CreateWoodpanel(GUIManager.PixelFix.transform, new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), new Vector2(0f, 0f), 400f, 300f);
+            panel.SetActive(true);
+        }
+
         // harmony patch to add a new hotkeybar to the players screen
         [HarmonyPatch(typeof(Hud), "Awake")]
         public static class Hud_Awake_Patch
         {
             public static void Postfix(Hud __instance)
             {
-                HotkeyBar hotKeyBarChildComponent = __instance.GetComponentInChildren<HotkeyBar>();
+                /*HotkeyBar hotKeyBarChildComponent = __instance.GetComponentInChildren<HotkeyBar>();
                 if (hotKeyBarChildComponent.transform.parent.Find("RPGHeimQuickCastBar") == null)
                 {
                     GameObject newHotKeyBar = Object.Instantiate(hotKeyBarChildComponent.gameObject, __instance.m_healthBarRoot, worldPositionStays: true);
@@ -29,13 +40,9 @@ namespace RPGHeim
                     newHotKeyBar.transform.localEulerAngles = new Vector3(0f, 0f, -90f);
                     newHotKeyBar.GetComponent<HotkeyBar>().m_selected = -1;
 
-                    foreach (HotkeyBar.ElementData element2 in newHotKeyBar.GetComponent<HotkeyBar>().m_elements)
-                    {
-                        Console.print("kk should do it now");
-                        if (element2 != null) Console.print(element2);
-                        Object.Destroy(element2.m_go);
-                    }
-                }
+                    
+                }*/
+                CreateClassBar();
             }
         }
     }
