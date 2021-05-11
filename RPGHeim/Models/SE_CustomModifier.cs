856efb94175b0f319d69f6b109fd30d7d38dab34
atravotum <44779531+atravotum@users.noreply.github.com>
2021-05-10 21:44:46 -0500

    disabling section I'm working on so I can merge latest with dev

--- RPGHeim/Models/SE_CustomModifier.cs
+++ RPGHeim/Models/SE_CustomModifier.cs
@@ -8,11 +8,11 @@ public class SE_CustomModifier : StatusEffect
 	public float m_parryModifier = 1f;
 	public Dictionary<Skills.SkillType, float> m_modSkills = new Dictionary<Skills.SkillType, float>();
 
-	public override void Setup(Character player)
+	/*public override void Setup(Character player)
 	{
 		foreach (KeyValuePair<Skills.SkillType, float> skillMod in m_modSkills)
 		{
-			player.RaiseSkill(skillMod.Key, skillMod.Value);
+			//player.RaiseSkill(skillMod.Key, skillMod.Value);
 		}
-	}
+	}*/
 }
